c4b6476c881cd24e6b2f9a9301eab29183c51974
Hugo <hugoperan@gmail.com>
2021-02-10 17:07:50 +0100

    Add Slug dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss';
 import { useState,useEffect } from 'react';
 import ModeContext from 'components/ModeContext/ModeContext';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from 'components/Navbar/Navbar';
 import Home from 'pages/Home/Home';
 import About from 'pages/About/About';
 import Works from 'pages/Works/Works';
+import Studycase from 'components/Studycase/Studycase';
 
 function App() {
   const [currentState, setCurrentState] = useState(localStorage.getItem("TheAgencyState") || "");
@@ -32,13 +33,15 @@ function App() {
             <Route path="/about">
               <About />
             </Route>
-            <Route path="/works">
+            <Route path="/works" exact>
               <Works />
+            </Route>
+            <Route path="/works/:slug" exact>
+              <Studycase />
             </Route>
           </Switch>
         </main>
       </Router>
-        <p>Coucou App</p>
       </div>
     </ModeContext.Provider>
   );
