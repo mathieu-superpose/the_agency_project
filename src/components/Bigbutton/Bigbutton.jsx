6da72ad397908bfe66964df5c0b83ca08aa4b197
Hugo <hugoperan@gmail.com>
2021-02-10 13:42:50 +0100

    Add Context

---
+++ src/components/Bigbutton/Bigbutton.jsx
@@ -0,0 +1,14 @@
+import './Bigbutton.scss';
+import ModeContext from 'components/ModeContext/ModeContext'
+
+const Bigbutton = () => {
+
+  const NameContext = createContext(defaultValue);
+
+  return (
+    <button>Test Bouton</button>
+  );
+};
+
+
+export default Bigbutton;
